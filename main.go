ae8a4f22d20b3d6b62bc5813613e84a1026df559
MaSz <maciej.sz@gmail.com>
2026-02-22 19:05:48 +0100

    #14 - refactor: use ldflags for version injection

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import "github.com/oxio/kvf/cmd"
 
+// version is set via ldflags during build: -X main.version=<version>
+var version = "dev"
+
 func main() {
+	cmd.SetVersion(version)
 	cmd.Execute()
 }
