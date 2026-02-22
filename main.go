78668b7605cd31ff08efe49e37ae8f2ee33f00f6
MaSz <maciej.sz@gmail.com>
2026-02-22 19:31:15 +0100

    #17 - feat: add release tasks and build info version

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
-import "github.com/oxio/kvf/cmd"
+import (
+	"runtime/debug"
+
+	"github.com/oxio/kvf/cmd"
+)
 
 // version is set via ldflags during build: -X main.version=<version>
 var version = "dev"
+
+func init() {
+	// If version wasn't set via ldflags, try to get it from build info
+	// This allows `go install` users to see a meaningful version
+	if version == "dev" {
+		if info, ok := debug.ReadBuildInfo(); ok {
+			for _, setting := range info.Settings {
+				if setting.Key == "vcs.revision" {
+					if len(setting.Value) >= 7 {
+						version = setting.Value[:7] // short commit hash
+					} else {
+						version = setting.Value
+					}
+					break
+				}
+			}
+		}
+	}
+}
 
 func main() {
 	cmd.SetVersion(version)
